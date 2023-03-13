a2461445edf9494de35be9cb624b96250206a3ac
jongchan <jckang568@gmail.com>
2023-03-13 14:14:30 +0900

    6.5 MVC프레임워크 구현 1단계 UserDao의 update, findAll 메서드 내부 구현

--- src/main/java/next/controller/UpdateUserController.java
+++ src/main/java/next/controller/UpdateUserController.java
@@ -3,6 +3,7 @@ package next.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import next.dao.UserDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +11,15 @@ import core.db.DataBase;
 import core.mvc.Controller;
 import next.model.User;
 
+import java.sql.SQLException;
+
 public class UpdateUserController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(UpdateUserController.class);
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        User user = DataBase.findUserById(req.getParameter("userId"));
+        UserDao userDao = new UserDao();
+        User user = userDao.findByUserId(req.getParameter("userId"));
         if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
             throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
         }
@@ -23,7 +27,11 @@ public class UpdateUserController implements Controller {
         User updateUser = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
                 req.getParameter("email"));
         log.debug("Update User : {}", updateUser);
-        user.update(updateUser);
+        try {
+            userDao.update(updateUser);
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+        }
         return "redirect:/";
     }
 }
